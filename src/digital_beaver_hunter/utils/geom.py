4653edd8b5669bcede3a4131156fc07e1a701833
Ingmar Nitze <ingmarnitze@gmail.com>
2025-03-11 13:31:35 +0100

    code formatting

--- src/digital_beaver_hunter/utils/geom.py
+++ src/digital_beaver_hunter/utils/geom.py
@@ -1,6 +1,6 @@
 import numpy as np
-from shapely.geometry import Point, Polygon
 import pandas as pd
+from shapely.geometry import Point, Polygon
 
 
 def validate_sides(coords):
@@ -9,7 +9,7 @@ def validate_sides(coords):
     )
 
 
-def sort_vertices(coords: np.array, yaw: float, tolerance=3, verbose:bool=False):
+def sort_vertices(coords: np.array, yaw: float, tolerance=3, verbose: bool = False):
     """
     Sort coordinates of a polygon based on the yaw angle.
 
@@ -24,56 +24,56 @@ def sort_vertices(coords: np.array, yaw: float, tolerance=3, verbose:bool=False)
         numpy.ndarray: Sorted coordinates of the polygon.
     """
     yaw = float(yaw)
-    if yaw >= 0+tolerance and yaw < 90-tolerance:
+    if yaw >= 0 + tolerance and yaw < 90 - tolerance:
         ul = coords[:, 1].argmax()
         ur = coords[:, 0].argmax()
         lr = coords[:, 1].argmin()
         ll = coords[:, 0].argmin()
-    elif yaw >= 90+tolerance and yaw < 180-tolerance:
+    elif yaw >= 90 + tolerance and yaw < 180 - tolerance:
         ul = coords[:, 0].argmax()
         ur = coords[:, 1].argmin()
         lr = coords[:, 0].argmin()
         ll = coords[:, 1].argmax()
-    elif yaw >= 180+tolerance and yaw < 270-tolerance:
+    elif yaw >= 180 + tolerance and yaw < 270 - tolerance:
         ul = coords[:, 1].argmin()
         ur = coords[:, 0].argmin()
         lr = coords[:, 1].argmax()
         ll = coords[:, 0].argmax()
-    elif yaw >= 270+tolerance and yaw <= 360-tolerance:
+    elif yaw >= 270 + tolerance and yaw <= 360 - tolerance:
         ul = coords[:, 0].argmin()
         ur = coords[:, 1].argmax()
         lr = coords[:, 0].argmax()
         ll = coords[:, 1].argmin()
-    elif yaw >= 360-tolerance or yaw < 0+tolerance:
-        df = pd.DataFrame(data=coords, columns=['x', 'y', 'z']).sort_values('y')
-        upper =df.iloc[:2].sort_values('x')
-        lower =df.iloc[2:].sort_values('x', ascending=False)
+    elif yaw >= 360 - tolerance or yaw < 0 + tolerance:
+        df = pd.DataFrame(data=coords, columns=["x", "y", "z"]).sort_values("y")
+        upper = df.iloc[:2].sort_values("x")
+        lower = df.iloc[2:].sort_values("x", ascending=False)
         ul, ur, lr, ll = pd.concat([upper, lower]).index.values
-    elif yaw >= 90-tolerance and yaw < 90+tolerance:
-        df = pd.DataFrame(data=coords, columns=['x', 'y', 'z']).sort_values('x', ascending=False)
-        right = df.iloc[:2].sort_values('y', ascending=False)
-        left = df.iloc[2:].sort_values('y')
+    elif yaw >= 90 - tolerance and yaw < 90 + tolerance:
+        df = pd.DataFrame(data=coords, columns=["x", "y", "z"]).sort_values(
+            "x", ascending=False
+        )
+        right = df.iloc[:2].sort_values("y", ascending=False)
+        left = df.iloc[2:].sort_values("y")
         ul, ur, lr, ll = pd.concat([right, left]).index.values
-    elif yaw >= 180-tolerance and yaw < 180+tolerance:
-        df = pd.DataFrame(data=coords, columns=['x', 'y', 'z']).sort_values('y')
-        upper =df.iloc[:2].sort_values('x')
-        lower =df.iloc[2:].sort_values('x', ascending=False)
+    elif yaw >= 180 - tolerance and yaw < 180 + tolerance:
+        df = pd.DataFrame(data=coords, columns=["x", "y", "z"]).sort_values("y")
+        upper = df.iloc[:2].sort_values("x")
+        lower = df.iloc[2:].sort_values("x", ascending=False)
         ul, ur, lr, ll = pd.concat([lower, upper]).index.values
-    elif yaw >= 270-tolerance and yaw < 270+tolerance:
-        df = pd.DataFrame(data=coords, columns=['x', 'y', 'z']).sort_values('x')
-        left = df.iloc[:2].sort_values('y')
-        right = df.iloc[2:].sort_values('y', ascending=False)
+    elif yaw >= 270 - tolerance and yaw < 270 + tolerance:
+        df = pd.DataFrame(data=coords, columns=["x", "y", "z"]).sort_values("x")
+        left = df.iloc[:2].sort_values("y")
+        right = df.iloc[2:].sort_values("y", ascending=False)
         ul, ur, lr, ll = pd.concat([left, right]).index.values
     else:
         raise ValueError("Yaw angle out of range")
-
 
     order = [ul, ur, lr, ll]
     if verbose:
         print(order)
 
     new_coords = coords[order]
-
 
     valid_direction = validate_sides(new_coords)
     if verbose:
