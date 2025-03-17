07534e15cf921511fee1caffc68fbf30903ca14c
Ingmar Nitze <ingmarnitze@gmail.com>
2025-03-17 09:22:37 +0100

    changed sort location

--- src/digital_beaver_hunter/scripts/inference.py
+++ src/digital_beaver_hunter/scripts/inference.py
@@ -135,7 +135,11 @@ def main(
 ):
     data_dir = basedir_data
     dirlist = list(data_dir.glob("*"))
-    projects = [d.name for d in dirlist if d.is_dir()].sort()
+    
+    projects = [d.name for d in dirlist if d.is_dir()]
+    
+    # sort project list
+    projects.sort()
 
     if projects_to_run:
         projects_run = projects_to_run
